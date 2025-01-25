8d00feb8a7d0580cacc3389db6d556a86adb16e2
SlyVenegas <venegasly@gmail.com>
2025-01-25 14:23:44 -0500

    REVERSION COMMIT: cambio en la gestion y carga de imagenes

--- components/product-image.tsx
+++ components/product-image.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { motion } from 'framer-motion'; // Asegúrate de que estás usando la importación correcta
+import { motion } from 'motion/react';
 import { Product } from '@/lib/products';
 
 interface ProductImageProps {
@@ -13,7 +13,6 @@ interface ProductImageProps {
 
 export function ProductImage({
   product,
-  priority = false,
   maxWidth = '100%',
   maxHeight = 'none',
   className = '',
@@ -37,12 +36,8 @@ export function ProductImage({
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         className="object-contain transition-opacity duration-200 aspect-square"
-        loading={priority ? 'eager' : 'lazy'}
-        decoding="async"
-        placeholder="blur"
-        blurDataURL={product.blurDataURL || '/path-to-blur-image.jpg'} // Asegúrate de tener una URL para la imagen borrosa
-        quality={75}
-        unoptimized={false}
+        loading="eager"
+        decoding="sync"
       />
     </motion.div>
   );
