f02f59e1bb008760f30d70e09344d25bdcd41ae4
SlyVenegas <venegasly@gmail.com>
2025-01-25 14:51:23 -0500

    gestion local de imagenes (temporal)

--- lib/products.ts
+++ lib/products.ts
@@ -9,157 +9,157 @@ export const products: Product[] = [
     id: 'sh-01-white',
     name: 'T-SHIRT 01',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/k8lhtviemd80kzzhyban',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-02-white',
     name: 'T-SHIRT 02',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/mcdta9zb7ar0mxg6ac9r',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-03-white',
     name: 'T-SHIRT 03',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/nkz9iunjuvojvtwz7duv',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-04-white',
     name: 'T-SHIRT 04',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/nykxnocsgobyog1x2p0u',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-05-white',
     name: 'T-SHIRT 05',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/vcaveyiibwxnjpspll5u',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-06-white',
     name: 'T-SHIRT 06',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/unyrdnskgzfvdrmy3cst',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-01-black',
     name: 'CREWNECK 01',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/k3gncud9ww8uvzqtpkvq',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-02-black',
     name: 'CREWNECK 02',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/gawwlevckg5fga2uugqs',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-07-white',
     name: 'T-SHIRT 07',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/lrynaepaiznde5pcira8',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-08-white',
     name: 'T-SHIRT 08',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/izrlzzght8dfh1yfvh3q',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-09-white',
     name: 'T-SHIRT 09',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/ziodirct09nj9rjfkfc1',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-10-white',
     name: 'T-SHIRT 10',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/wns3rznloefamc9jts3g',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-11-white',
     name: 'T-SHIRT 11',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/o4wc7itudynht2df6heo',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-12-white',
     name: 'T-SHIRT 12',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/wns3rznloefamc9jts3g',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-03-black',
     name: 'CREWNECK 03',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/tpgdor7grcwsv7k6sh42',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-04-black',
     name: 'CREWNECK 04',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/zvcxjsszc3ejyk9c7ike',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-05-black',
     name: 'CREWNECK 05',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/o9hibglpppqozh05tsbk',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-06-black',
     name: 'CREWNECK 06',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/vzcxy1v6diue859mrd05',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-07-black',
     name: 'CREWNECK 07',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/qnc0h7fzobwnmazfx6c4',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-08-black',
     name: 'CREWNECK 08',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/a6ni4hyymeprvyazxapz',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-09-black',
     name: 'CREWNECK 09',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/clsbmmlsautfzctwybuz',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-10-black',
     name: 'CREWNECK 10',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/gtarp9rszznmg80kcckf',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-13-white',
     name: 'T-SHIRT 13',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/yz51rn7zyzuztxefxc1b',
+      '/images/merchTest.png',
   },
   {
     id: 'sh-14-white',
     name: 'T-SHIRT 14',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/e7iicxrne84y259mical',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-11-black',
     name: 'CREWNECK 11',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/ndrj0c4ivso01b2f80ds',
+      '/images/merchTest.png',
   },
   {
     id: 'hd-12-black',
     name: 'CREWNECK 12',
     image:
-      'https://res.cloudinary.com/ddqgjf5wr/image/upload/f_auto,q_auto/v1/merch_bluk/cfj9ryqb7vsgh3lf1ngg',
+      '/images/merchTest.png',
   },
 ];
 
